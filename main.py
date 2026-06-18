1b3739d0fd76eec198b2a844574f2e03f26b5438
Evan Reierson <evan.reierson@gmail.com>
2026-06-18 13:57:12 -0600

    basic raylib window

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import pyray as pr
+
+
 def main():
-    print("Hello from flock-field!")
+    pr.init_window(800, 450, "Hello Pyray")
+    pr.set_target_fps(60)
+
+    while not pr.window_should_close():
+        pr.begin_drawing()
+        pr.clear_background(pr.RAYWHITE)
+        pr.end_drawing()
+
+    pr.close_window()
 
 
 if __name__ == "__main__":
